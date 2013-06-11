8e4b872569e5dbb5516d9e3657306d1e16e1b185
Kyla Medina <hi@ky.la>
2013-06-11 17:17:42 -0400

    pivoting

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function(){
 
-
 	var parsePoints = function(datas){
 
 		var point = new Array();
@@ -21,6 +20,9 @@ jQuery(document).ready(function(){
 		jQuery(this).hover(
 
 		function(){
+			var id = jQuery(this).parent('svg').parent('span').attr('id').toUpperCase();
+			var svgClone = jQuery('#' + id + ' svg').clone();
+			var offset = jQuery(this).offset();
 
 			//var points = parsePoints(jQuery(this).attr('points'));
 
@@ -33,20 +35,52 @@ jQuery(document).ready(function(){
 			var distX = (this.getBoundingClientRect().width) - (this.getBoundingClientRect().width*1.2);
 			var distY = (this.getBoundingClientRect().height) - (this.getBoundingClientRect().height*1.2);
 
-			console.log(distX + " : " + distY);
+			//var distX = (offset.left) - (offset.left*1.2);
+			//var distY = (offset.top) - (offset.top*1.2);
 
-			jQuery(this).parent('g').append( jQuery(this).delay(10).transition({ scale: [1.2], duration: 10, x: distX, y: distY }) );
+			//console.log(/*distX + " : " + distY + " : " +*/);
+
+			jQuery(this).parent('svg').append( jQuery(this).delay(10).transition({ scale: [1.2], duration: 10, x: distX, y: distY }) );
 
 		},
 		function(){
-
+			var id = jQuery(this).parent('svg').parent('span').attr('id').toUpperCase();
+			
 			jQuery(this).transition({ scale: [1], duration: 10, x: 0, y: 0 }).dequeue();
+			jQuery('#' + id).replaceWith(svgClone);
+			//var id = jQuery(this).parent('svg').parent('span').attr('id').toLowerCase();
 
+			//
+			//jQuery(this).parent('svg').insertBefore(g#id).delay(10).transition({ scale: [1], duration: 10, x: distX, y: distY });
+			//jQuery(this).parent('svg').prepend();
+
+			console.log(id)//.insertBefore(g#id);
 
 		});
 
 	});
 
+	jQuery('.wrapper div').click(function() {
+
+		if(jQuery(this).hasClass('left'))
+			jQuery(this).parent('div').find('li.active').prev().addClass('active').click().siblings().removeClass('active').dequeue();
+
+		if(jQuery(this).hasClass('right'))
+			jQuery(this).parent('div').find('li.active').next().addClass('active').click().siblings().removeClass('active').dequeue();
+
+	});
+
+	jQuery('.wrapper nav ul li').click(function() {
+
+		var letter = (jQuery(this).html().toUpperCase() != '#') ? jQuery(this).html().toUpperCase() : 'hash';
+      	var target = jQuery("#"+letter);
+      
+      	if (target) {
+          	jQuery('html,body').animate({ scrollLeft: target.offset().left }, 500);
+          	jQuery(this).addClass('active').siblings().removeClass('active');
+          	return false;
+      	}
+	    
+	});
+
 });
-
-
