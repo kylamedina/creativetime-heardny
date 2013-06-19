6b33a57c37127909432ac671e123abf4fc95151d
Jonathan Delefortrie <jonathandelefortrie@gmail.com>
2013-06-19 21:28:03 +0200

    last update js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,21 +1,27 @@
 jQuery(document).ready(function(){
 
+	var x,y;
+
 	jQuery('#tweets').find('div').each(function(){
 		jQuery(this).children().contents().find('body div.media a img').css('width','auto').css('height','200px');
 		jQuery(this).children().contents().find('body div.media iframe').css('width','auto').css('height','200px');
 	});
 
-	jQuery("#close").click(function () { 
+	jQuery("#overlay").click(function () { 
 
-		jQuery(this).parent('#overlay').data('current','').addClass('overlay').fadeOut('fast').children('span').empty().dequeue();
-		jQuery('#tweets').css({ opacity: 0 }).find('div').each(function(){jQuery(this).css('display', 'none');});
+		jQuery(this).children('#close').trigger('click');
 
 	});
 
-	jQuery("#overlay").click(function () { 
+	jQuery("#close").click(function () {
 
-		jQuery(this).data('current','').addClass('overlay').fadeOut('fast').children('span').empty().dequeue();
-		jQuery('#tweets').css({ opacity: 0 }).find('div').each(function(){jQuery(this).css('display', 'none');});
+		var current = jQuery(this).parent('#overlay').data('current');
+
+		jQuery('body').find('object').each(function(){
+
+			jQuery(this.getSVGDocument().documentElement).children('g').children('g#'+current).trigger('click');
+
+		});
 
 	});
 
@@ -25,95 +31,65 @@ jQuery(document).ready(function(){
 
 			jQuery(this).click(function(e){
 
-				var x = e.pageX + jQuery('#'+this.parentNode.getAttribute('id')).offset().left;
-				var y = e.pageY + jQuery('#'+this.parentNode.getAttribute('id')).offset().top;
-
-				var img = '';
-				var id = this.getAttribute('id');
-
 				jQuery('.meter').fadeIn('fast');
 
-				if(jQuery(this).attr('class') == 'twitter') var tweet = true;
-					
-				if (jQuery(this).attr('class') == 'vine')
+				var i = this.getAttribute('id');
+				var c = this.getAttribute('class');
+
+				if(!e.isTrigger)
+					x = e.pageX + jQuery('#'+this.parentNode.getAttribute('id')).offset().left, y = e.pageY + jQuery('#'+this.parentNode.getAttribute('id')).offset().top;
+
+				if(c == 'vine')
 					var img = '<iframe class="vine-embed" src="'+jQuery(this).attr('data-video')+'/embed/simple" width="400" height="400" frameborder="0"></iframe>';
+				
 				else
-					var img = '<img class="scaled-img" src="assets/img/'+this.parentNode.getAttribute('id')+'/big/'+id+'.jpg">';
-			
-				if(jQuery('#overlay').data('current') != id && jQuery('#overlay').data('current')) {
+					var img = '<img class="scaled-img" src="assets/img/'+this.parentNode.getAttribute('id')+'/big/'+i+'.jpg">';
 
-					jQuery('#tweets').css({ opacity: 0 }).find('div').each(function(){jQuery(this).css('display', 'none');})
-
+				if(jQuery('#overlay').hasClass('overlay')) 
 					jQuery('#overlay')
-							     .data('current',id)
-								 .stop()
-								 .removeClass('overlay')
-								 .children('span')
-								 .empty()
-								 .parent('div')
-								 .fadeIn('fast')
-								 .children('span')
-								 .append(img)
-								 .css({ opacity: 0 })
-								 .children().load(function(){
+									 .data('current', i)
+									 .stop()
+									 .removeClass('overlay')
+									 .fadeIn('fast')
+									 .children('span')
+									 .append(img)
+									 .css({ opacity: 0 })
+									 .children().load(function(){
 
-								 	jQuery('.meter').css('display', 'none');
+									 	jQuery('div.meter').css('display', 'none');
 
-								 	if(tweet) {
-										var w = 229;
-										var h = (jQuery('#overlay').height()/2) - (jQuery('#tweets').children('#'+id).height()/2);
-										jQuery('#tweets').addClass('importantRule').css({ opacity: 1, translate: [w,h] }).children('#'+id).fadeIn('fast').siblings().css('display', 'none');
-								 	} else {
-								 		var w = (jQuery('#overlay').width()/2) - (jQuery('#popup').outerWidth()/2);
-										var h = (jQuery('#overlay').height()/2) - (jQuery('#popup').outerHeight()/2);
-								 		jQuery(this).parent('span').fadeIn('fast').css({ opacity: 1, translate: [w,h] });
-								 	}
-								 })
-								 .dequeue();
+										if(c == 'twitter') {
+											var w = 229;
+											var h = (jQuery('#overlay').height()/2) - (jQuery('#tweets').children('#'+i).height()/2);
+											jQuery('#tweets').addClass('importantRule').css({ opacity: 1, translate: [w,h] }).children('#'+i).fadeIn('fast').siblings().css('display', 'none');
+									 	} else {
+									 		var w = (jQuery('#overlay').width()/2) - (jQuery('#popup').outerWidth()/2);
+											var h = (jQuery('#overlay').height()/2) - (jQuery('#popup').outerHeight()/2);
+									 		jQuery(this).parent('span').fadeIn('fast').css({ opacity: 1, translate: [w,h] });
+									 	}
 
-				} else 
-					if(jQuery('#overlay').hasClass('overlay')) 
-						jQuery('#overlay')
-										 .data('current', id)
-										 .stop()
-										 .removeClass('overlay')
-										 .fadeIn('fast')
-										 .children('span')
-										 .append(img)
-										 .css({ opacity: 0 })
-										 .children().load(function(){
+									 })
+									 .dequeue();
 
-										 	jQuery('.meter').css('display', 'none');
-
-											if(tweet) {
-												var w = 229;
-												var h = (jQuery('#overlay').height()/2) - (jQuery('#tweets').children('#'+id).height()/2);
-												jQuery('#tweets').addClass('importantRule').css({ opacity: 1, translate: [w,h] }).children('#'+id).fadeIn('fast').siblings().css('display', 'none');
-										 	} else {
-										 		var w = (jQuery('#overlay').width()/2) - (jQuery('#popup').outerWidth()/2);
-												var h = (jQuery('#overlay').height()/2) - (jQuery('#popup').outerHeight()/2);
-										 		jQuery(this).parent('span').fadeIn('fast').css({ opacity: 1, translate: [w,h] });
-										 	}
-										 })
-										 .dequeue();
-
-					else jQuery('#overlay')
-										 .data('current','')
-										 .stop()
-										 .addClass('overlay')
-										 .fadeOut('fast')
-										 .children('span')
-										 .transition({ translate: [x,y] }, 500)
-										 .empty()
-										 .parent('div')
-										 .children('div.meter').css('display', 'none')
-										 .parent('div')
-										 .parent('body')
-										 .children('#tweets')
-										 .css({ opacity: 0 })
-										 .find('div')
-										 .each(function(){jQuery(this).css('display', 'none');})
-										 .dequeue();
+				else jQuery('#overlay')
+									 .data('current','')
+									 .stop()
+									 .addClass('overlay')
+									 .fadeOut('fast')
+									 .children('div.meter')
+									 .css('display', 'none')
+									 .parent('div')
+									 .children('span')
+									 .transition({ translate: [x,y] }, 500)
+									 .empty()
+									 .parent('div')
+									 .parent('body')
+									 .children('#tweets')
+									 .removeClass('importantRule')
+									 .css({ opacity: 0 })
+									 .find('div')
+									 .each(function(){jQuery(this).css('display', 'none');})
+									 .dequeue();
 
 				
 			});
@@ -125,6 +101,7 @@ jQuery(document).ready(function(){
 			function(){
 				jQuery(this).transition({ opacity: 1 }, 50).dequeue();
 			});
+
 		});
 
 	});
